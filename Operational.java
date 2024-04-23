2a7f371b341b4b1009478d0242c24ad18828d398
kasanzekasanze <167344622+kasanzekasanze@users.noreply.github.com>
2024-04-23 10:53:24 +0300

    Add files via upload

---
+++ Operational.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conditional_statement;
+/**
+ *
+ * @author cleop
+ */
+public class Operational {
+    
+    int a;
+    int b;
+    
+    void add(){
+    int sum;
+    sum=a+b;
+    System.out.print("The sum ="+sum);
+    }
+    void sub(){
+        int diff;
+        diff=a-b;
+    System.out.print("The difference ="+diff);
+    }
+    void mult(){
+        int pro;
+        pro=a*b;
+    System.out.print("The product ="+pro);
+    }
+    void div(){
+        int rat;
+        rat=a/b;
+    System.out.print("The division ="+rat);
+    }
+          
+}
