fc0ae199c275493eb112de20b0c70083e8e74926
KABAITILAKI <167412185+KABAITILAKI@users.noreply.github.com>
2024-04-23 10:41:17 +0300

    Add files via upload

---
+++ conditional.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conditional_statement;
+/**
+ * 
+ *
+ * @author cleop
+ */
+public class conditional {
+    int c;
+
+         
+
+    void display() {
+        if(c%2==0){
+            System.out.print("The number is even.\n");
+        }
+        else {
+         System.out.print("The number is odd.\n");
+}   
+    
+    }}
